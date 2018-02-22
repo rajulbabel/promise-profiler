e2812fc1e2fb34496ff270d7d620af0c22256a10
rajulbabel <babelrajul@gmail.com>
2018-02-22 19:58:22 +0530

    update error file

--- src/ErrorLib.js
+++ src/ErrorLib.js
@@ -3,15 +3,19 @@
 const errorMap = {
 
 	PromiseTypeError: {
+		errorType: TypeError,
 		message: 'expecting a bluebird promise'
+	},
+	PromiseNotFound: {
+		errorType: ReferenceError,
+		message: 'no bluebird promise library found, make sure that you have bluebird dependency in package.json'
 	}
 
 };
 
-const throwError = {
-	PromiseTypeError: () => {
-		throw new TypeError(errorMap.PromiseTypeError.message);
-	}
+const throwError = function throwError (error) {
+
+	throw new error.errorType(error.message);
 };
 
 module.exports = {
